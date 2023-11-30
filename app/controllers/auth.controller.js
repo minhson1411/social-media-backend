9ad93aca63e13d0015c7ec6922cbbc2836bc3103
minhson1411 <nguyenminhson1427@gmail.com>
2023-11-30 23:38:24 +0700

    (feat) add ESLint

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -1,37 +1,14 @@
 
+const HTTPStatusCode = new (require('../common/constants/HttpStatusCode.js'))();
 const query = require('../modules/user.query.js');
-const { comparePassword, generateToken } = require('../services/auth.service.js');
+const { generateToken, verifyToken } = require('../services/auth.service.js');
 
 exports.auth = {
     login: async (req, res) => {
-        const email = req.body.email.toLowerCase();
-        const password = req.body.password;
-        const dataQuery = await query.getUsers(email);
-        if (dataQuery.length === 0) {
-            res.status(401).send('Invalid email or password');
-        }
-        const user = dataQuery.data[0];
-        // if (comparePassword(password, user.password)) {
-        if (password !== user.password) {
-            res.status(401).send('Invalid email or password');
-        }
-        const payload = {
-            id: user.id,
-        };
-        const accessToken = generateToken(payload, process.env.ACCESS_TOKEN_SECRET, process.env.ACCESS_TOKEN_LIFE);
-        if (!accessToken) {
-            res.status(401).send('Login failed, please try again');
-        }
-        let refreshToken = generateToken(payload, process.env.REFRESH_TOKEN_SECRET, process.env.REFRESH_TOKEN_LIFE);
-        if (!user.refreshToken) {
-            await query.updateRefreshToken(user.id, refreshToken).catch(err => {
-                console.error(err);
-                res.status(401).send('Login failed, please try again');
-            });
-        } else {
-            refreshToken = user.refreshToken;
-        }
-        res.status(200).json({
+        const accessToken = req.accessToken;
+        const refreshToken = req.refreshToken;
+        const user = req.user;
+        res.status(HTTPStatusCode.OK).json({
             'message': 'Login successful',
             'accessToken': accessToken,
             'refreshToken': refreshToken,
@@ -39,27 +16,35 @@ exports.auth = {
         });
     },
     register: (req, res) => {
-        res.send('register ok');
+        res.status(HTTPStatusCode.OK).json({
+            'message': 'Register successful'
+        });
     },
     verify: (req, res) => {
-        res.send('verify ok');
+        res.status(HTTPStatusCode.OK).json({
+            'message': 'Verify successful'
+        });
     },
     refresh: (req, res) => {
         const refreshToken = req.body.refreshToken;
         const payload = verifyToken(refreshToken, process.env.REFRESH_TOKEN_SECRET);
         if (!payload) {
-            res.status(401).send('Invalid refresh token');
+            res.status(HTTPStatusCode.Unauthorized).send('Invalid refresh token');
         }
         const accessToken = generateToken(payload, process.env.ACCESS_TOKEN_SECRET, process.env.ACCESS_TOKEN_LIFE);
         if (!accessToken) {
-            res.status(401).send('Invalid refresh token');
+            res.status(HTTPStatusCode.Unauthorized).send('Invalid refresh token');
         }
-        res.status(200).json({
+        res.status(HTTPStatusCode.OK).json({
             'message': 'Refresh token successful',
             'accessToken': accessToken
         });
     },
-    logout: (req, res) => {
-        res.send('logout ok');
+    logout: async (req, res) => {
+        const id = req.body.id;
+        await query.updateRefreshToken(id, null).catch(err => {
+            console.error(err);
+            res.status(HTTPStatusCode.Unauthorized).send('Logout failed, please try again');
+        });
     }
 };
