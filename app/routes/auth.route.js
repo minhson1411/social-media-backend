4a7dc5d6b8c6a6839eec0d6e3fbaf015ac611c04
minhson1411 <nguyenminhson1427@gmail.com>
2023-12-08 15:07:29 +0700

    (fix) logout and refresh token

--- app/routes/auth.route.js
+++ app/routes/auth.route.js
@@ -1,36 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const { auth } = require('../controllers/auth.controller.js');
-const { validateReqBody, authenticatePassword, generateTokens } = require('../middlewares/validateRequest.js');
-// swager docs
-/**
- * @swagger 
- * /auth/login:
- * post:
- * description: Login
- * parameters:
- * - name: email
- *  description: Email
- * in: formData
- * required: true
- * type: string
- * - name: password
- * description: Password
- * in: formData
- * required: true
- * type: string
- * responses:
- * 200:
- * description: Login successful
- * 401:
- * description: Invalid email or password
- * 500:
- * description: Login failed, please try again
- **/
+const { validateReqBody, isValidID } = require('../middlewares/requestValidate.js');
+const { authenticatePassword, generateTokens } = require('../middlewares/authValidate.js');
+
 router.post('/login', validateReqBody, authenticatePassword, generateTokens, auth.login);
 router.post('/register', validateReqBody, auth.register);
 router.post('/verify', auth.verify);
 router.post('/refresh', auth.refresh);
-router.post('/logout', auth.logout);
+router.post('/logout', isValidID, auth.logout);
 
 module.exports = router;
