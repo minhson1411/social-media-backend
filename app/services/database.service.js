0e8107b17fef31c6ece99890e29de3b163edb526
minhson1411 <nguyenminhson1427@gmail.com>
2023-12-08 22:35:41 +0700

    add .env

--- app/services/database.service.js
+++ app/services/database.service.js
@@ -6,7 +6,6 @@ const conn = db.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  dateStrings: true,
 });
 
 conn.connect((err) => {
